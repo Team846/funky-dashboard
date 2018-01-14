a23bd4060cbf04cc96d12c87d7594606b0095a6c
Aanand Kainth <akainth015@gmail.com>
2018-01-13 16:20:40 -0800

    Check if any datasets already have the same name as the one being added. If there is already a dataset with the same name, an IllegalArgumentException will be thrown.

--- jvm/src/main/scala/com/lynbrookrobotics/funkydashboard/DatasetGroup.scala
+++ jvm/src/main/scala/com/lynbrookrobotics/funkydashboard/DatasetGroup.scala
@@ -4,10 +4,15 @@ class DatasetGroup(name: String) {
   private var datasets: List[Dataset[_]] = List.empty
 
   def properties = DatasetGroupDefinition(name, datasets.map(_.definition))
+
   def currentValue: Map[String, String] = datasets.map(d => d.definition.name -> d.currentValueJSON).toMap
+
   def dataset(name: String): Option[Dataset[_]] = datasets.find(_.definition.name == name)
 
   def addDataset(dataset: Dataset[_]): Unit = {
+    if (datasets.map(_.definition.name).contains(dataset.definition.name)) {
+      throw new IllegalArgumentException(s"Overriding name of existing DatasetGroup(${this.properties.name})")
+    }
     datasets = datasets :+ dataset
   }
 }
